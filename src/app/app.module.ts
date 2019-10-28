7b832dcbf4f0621ecba59f392f7e32c8279cda77
Edson Camargo Menezes <42123898+edsoncamargo@users.noreply.github.com>
2019-10-28 09:18:19 -0300

    .
    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,16 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './ui/components/navbar/navbar.component';
 import { LoginComponent } from './ui/pages/login/login.component';
+import { WebComponent } from './ui/pages/web/web.component';
 import { UserCadastreComponent } from './ui/pages/user-cadastre/user-cadastre.component';
+import { UserEditComponent } from './ui/components/user-edit/user-edit.component';
 import { WebDashboardComponent } from './ui/pages/web-dashboard/web-dashboard.component';
 import { AdminCadastreComponent } from './ui/pages/web-dashboard/admin-cadastre/admin-cadastre.component';
 import { GameCadastreComponent } from './ui/pages/web-dashboard/game-cadastre/game-cadastre.component';
-import { WebComponent } from './ui/pages/web/web.component';
 import { GameListComponent } from './ui/components/game-list/game-list.component';
 import { GameDetailsComponent } from './ui/components/game-details/game-details.component';
+import { ProfileComponent } from './ui/pages/web/profile/profile.component';
+import { FooterComponent } from './ui/components/footer/footer.component';
 
 // Pipes
 import { HasImagePipe } from './pipes/has-image.pipe';
@@ -21,14 +24,13 @@ import { HasImagePipe } from './pipes/has-image.pipe';
 // Services
 import { SettingsService } from './services/settings.service';
 import { AuthenticationService } from './services/authentication.service';
+import { LocalStorageService } from './services/local-storage.service';
+import { UserService } from './services/user.service';
 import { GameService } from './services/game.service';
 
 // Locale
 import localept from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
-import { UserEditComponent } from './ui/components/user-edit/user-edit.component';
-import { ProfileComponent } from './ui/pages/web/profile/profile.component';
-import { FooterComponent } from './ui/components/footer/footer.component';
 registerLocaleData(localept, (settingsService) => settingsService.getLocale());
 
 @NgModule({
@@ -61,6 +63,8 @@ registerLocaleData(localept, (settingsService) => settingsService.getLocale());
       useFactory: (settingsService) => settingsService.getLocale()
     },
     AuthenticationService,
+    LocalStorageService,
+    UserService,
     GameService
   ],
   bootstrap: [AppComponent]
